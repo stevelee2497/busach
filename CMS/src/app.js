a0e0f91b04769bcc79fe68e037e484fc06644e7a
Quoc Tran <Quoc.Tran@sioux.asia>
2019-09-03 10:53:43 +0700

    Handle Error

--- CMS/src/app.js
+++ CMS/src/app.js
@@ -1,10 +1,25 @@
 import { reducer as formReducer } from 'redux-form'
+import { notification } from 'antd';
+import router from 'umi/router';
 
 export const dva = {
   config: {
     onError(err) {
       err.preventDefault();
-      console.error(err.response);
+      if (err.response) {
+        notification['error']({
+          message: 'Ops',
+          description: err.response.data.errorMessage,
+        })
+        router.push('/500');
+      } else {
+        notification['error']({
+          message: 'Ops',
+          description: err.message,
+        });
+        router.push('/404');
+      }
+
     },
     extraReducers: {
       form: formReducer,
